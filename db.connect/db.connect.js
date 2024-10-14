f50c2255b444c0567cacc1d7b282f6c4cdbc5141
NeerajNero <neerajnero9066@gmail.com>
2024-10-14 13:10:45 +0530

    Update db.connect.js
    updated uri with atlas cloud

--- db.connect/db.connect.js
+++ db.connect/db.connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = "mongodb://localhost:27017/";
+const mongoURI = "mongodb://nero:nero@neog-shard-00-00.ghizv.mongodb.net:27017,neog-shard-00-01.ghizv.mongodb.net:27017,neog-shard-00-02.ghizv.mongodb.net:27017/?ssl=true&replicaSet=atlas-xvxmq6-shard-0&authSource=admin&retryWrites=true&w=majority&appName=neoG";
 
 const initializeDatabase = async () => {
     try{
